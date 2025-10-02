755655b32866ad1a0ac8966e06c50d5ffa9ebae1
visista19 <swarangivisista@gmail.com>
2025-10-03 02:05:13 +0530

    new

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,7 +12,7 @@ function App() {
     setError("");
     setWeather(null);
     try {
-      const response = await axios.post("http://localhost:5000/api/requests", {
+      const response = await axios.post("https://weather-app-1-20i9.onrender.com/api/requests", {
         location: loc,
       });
       setWeather(response.data);
